8324232009315480c2bc406aa1e815fdb0d8648e
nagesh <nageshgadupudi@gmail.com>
2018-06-29 06:12:15 +0000

    fixed error

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,7 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
 if node["platform"] == "ubuntu"
 	execute "apt-get update -y" do
 	end
@@ -12,43 +11,43 @@ end
 #include_recipe "php::default"
 
 package "httpd" do
-		package_name node["apache"]["package"]
+	package_name node["apache"]["package"]
 end
 
 node["apache"]["sites"].each do |sitename, data|
-        document_root = "/content/sites/#{sitename}"
-
-        directory document_root do
-                        mode "0755"
-                        recursive true
-        end
+	document_root = "/content/sites/#{sitename}"
+	
+	directory document_root do
+		mode "0755"
+		recursive true
+	end
 	
 	if node["platform"] == "ubuntu"
-			template_location = "/etc/apache2/sites-enabled/#{sitename}.conf"
+		template_location = "/etc/apache2/sites-enabled/#{sitename}.conf"
 	elsif node["platform"] == "amazon"
-			template_location = "/etc/httpd/conf.d/#{sitename}.conf"
+		template_location = "/etc/httpd/conf.d/#{sitename}.conf"
+	end
+	
+	template template_location do
+		source "vhost.erb"
+		mode "0644"
+		variables(
+			:document_root => document_root,
+			:port => data["port"],
+			:domain => data["domain"]
+		)
+		notifies :restart, "service[httpd]"
 	end
 
-        template template_location do
-                source "vhost.erb"
-                mode "0644"
-                variables(
-                        :document_root => document_root,
-                        :port => data["port"],
-                        :domain => data["domain"]
-                        )
-                        notifies :restart, "service[httpd]"
-        end
-		template "/content/sites/#{sitename}/index.html" do
-                source "index.html.erb"
-                mode "0644"
-                variables
-			(
-                        :site_title => data["site_title"],
-                        :comingsoon => "still cooking",
-                        :author_name => node["author"]["name"]
-			)
-                end
+	template "/content/sites/#{sitename}/index.html" do
+	source "index.html.erb"
+	mode "0644"
+	variables(
+		:site_title => data["site_title"],
+		:comingsoon => "still cooking",
+		:author_name => node["author"]["name"]
+		)
+	end
 end
 
 execute "rm /etc/httpd/conf.d/welcome.conf" do
@@ -59,13 +58,12 @@ execute "rm /etc/httpd/conf.d/welcome.conf" do
 end
 
 execute "rm /etc/httpd/conf.d/README" do
-        only_if do
-                File.exist?("/etc/httpd/conf.d/README")
-        end
+	only_if do
+		File.exist?("/etc/httpd/conf.d/README")
+	end
 end
 
-
 service "httpd" do
-		service_name node["apache"]["package"]
-                action [:enable, :start]
+	service_name node["apache"]["package"]
+        action [:enable, :start]
 end
